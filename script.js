0d91e16831c4e03c008ceaee4868b3deb1be38ca
Arauco <arishel@gmail.com>
2020-01-21 16:52:06 -0800

    added project Thats Da Burger

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 
 
 
+// $(document).ready(function () {
+//     for (let i = 0; i < projectsArr.length; i++) {
+//         // make new column div for portfolio thumbnails
+//         const column = $('<div>');
+//         column.addClass('column')
+//         $('#portThumbs').append(column);
+//         // insert image into each column
+//         const image = $('<img>')
+//         image.addClass('project-pic');
+//         image.attr('src', projectsArr[i].imageLoc)
+//         image.attr('alt', projectsArr[i].tittle)
+//         image.attr('id', i)
+//         // attach click function
+//         image.attr('onclick', 'expandPic(this);')
+//         column.append(image);
+//         }
+//  })
+
 $(document).ready(function () {
     for (let i = 0; i < projectsArr.length; i++) {
         // make new column div for portfolio thumbnails
-        const column = $('<div>');
-        column.addClass('column')
-        $('#portThumbs').append(column);
-        // insert image into each column
-        const image = $('<img>')
-        image.addClass('project-pic');
-        image.attr('src', projectsArr[i].imageLoc)
-        image.attr('alt', projectsArr[i].tittle)
-        image.attr('id', i)
-        // attach click function
-        image.attr('onclick', 'expandPic(this);')
-        column.append(image);
-        }
- 
-
+        $('#portThumbs').append(`<div class="column">
+        <img class="project-pic" src="${projectsArr[i].imageLoc}" alt="${projectsArr[i].tittle}" id="${i}" onclick="expandPic(this);">
+    </div>`);
+    };
 })
 
 
