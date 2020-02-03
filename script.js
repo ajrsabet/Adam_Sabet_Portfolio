375ccb4a2a229a1d857d38b29d04c6d0654f7d11
Arauco <arishel@gmail.com>
2020-02-03 14:33:14 -0800

    added skills bar

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-
 $(document).ready(function () {
+    // Display portfolio thumbnails
     for (let i = 0; i < projectsArr.length; i++) {
         // make new column div for portfolio thumbnails
         $('#portThumbs').append(`<div class="column">
         <img class="project-pic" src="${projectsArr[i].imageLoc}" alt="${projectsArr[i].tittle}" id="${i}" onclick="expandPic(this);">
     </div>`);
     };
+    // Display first project full view
     expandPic()
-})
 
+    // skill bar animation
+    $('.skillbar').each(function () {
+        $(this).find('.skillbar-bar').animate({
+            width: $(this).attr('data-percent')
+        }, 6000);
+    });
+});
 
+// Display first project full view
 function expandPic(imgs) {
-let project;
+    let project;
     if (!imgs) {
         project = projectsArr[0];
     } else {
         project = projectsArr[imgs.id];
     }
-    
+
     $(".project-viewer").empty();
     $(".project-viewer").html(`<img id="expandedImg" class="image" alt="project-viewer" src="${project.imageLoc}"
     style="width:100%">
